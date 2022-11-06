18670970141bd06d802b02f5b52cfda18b563f70
Darren Tuer <dtuer@tritium.com.au>
2022-11-06 13:52:37 +1000

    update references, update to new code for handling of Azure Vault secrets

--- src/Blogifier/Program.cs
+++ src/Blogifier/Program.cs
@@ -5,10 +5,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.IO;
 using System.Linq;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.AzureKeyVault;
+using System;
+using Azure.Identity;
 
 namespace Blogifier
 {
@@ -41,17 +40,7 @@ namespace Blogifier
                      if (context.HostingEnvironment.IsProduction())
                      {
                          var builtConfig = config.Build();
-
-                         var azureServiceTokenProvider = new AzureServiceTokenProvider();
-
-                         var keyVaultClient = new KeyVaultClient(
-                             new KeyVaultClient.AuthenticationCallback(
-                                 azureServiceTokenProvider.KeyVaultTokenCallback));
-
-                         config.AddAzureKeyVault(
-                             $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/",
-                             keyVaultClient,
-                             new DefaultKeyVaultSecretManager());
+                         config.AddAzureKeyVault(new Uri($"{builtConfig["KeyVaultName"]}.vault.azure.net/"), new DefaultAzureCredential());
                      }
                  })
                 .ConfigureWebHostDefaults(webBuilder =>
